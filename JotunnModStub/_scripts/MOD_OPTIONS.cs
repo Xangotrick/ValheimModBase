daca5ccc92b60b2564b0d780885fcf5f7b6cb863
Xangotrick <michael.arthur.montana@gmail.com>
2026-03-09 20:50:36 +0100

    Add files via upload

--- JotunnModStub/_scripts/MOD_OPTIONS.cs
+++ JotunnModStub/_scripts/MOD_OPTIONS.cs
@@ -10,4 +10,5 @@ internal class MOD_OPTIONS
     /// Est ce que le mod utilise les classes de synchronisation des données? 
     /// Si oui, implique que le mod ne fonctionne qu'avec un serveur dédié et un client
     public static bool IS_ONLINE_SYNC = true;
+    public static string MODNAME = "TemplateName";
 }
