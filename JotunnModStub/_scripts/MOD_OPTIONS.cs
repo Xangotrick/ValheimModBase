17a2eb159e0130eb54f214db8677f9c03104c1c4
Xangotrick <michael.arthur.montana@gmail.com>
2026-03-19 13:46:56 +0100

    Add files via upload

--- JotunnModStub/_scripts/MOD_OPTIONS.cs
+++ JotunnModStub/_scripts/MOD_OPTIONS.cs
@@ -9,10 +9,17 @@ internal class MOD_OPTIONS
 {
     /// Est ce que le mod utilise les classes de synchronisation des données? 
     /// Si oui, implique que le mod ne fonctionne qu'avec un serveur dédié et un client
-    public static bool IS_ONLINE_SYNC = true;
+    public static bool IS_ONLINE_SYNC = false;
 
-    public const string MODNAME = "SpellMod";
+    
+
+    public const string MODNAME = "Vivaldi";
 
     /// Certaines fonctions ne doivent pas être exécutées plusieurs fois à travers les différents mods. Normalement il ne faut pas toucher à cette valeur
     public static bool MASTER_MOD = false;
+
+    public static bool IS_CHEAT_MODE = true;
+    public static bool IS_GOD_MODE = true;
+
+    public static bool IS_IG_CONSOLE = true;
 }
